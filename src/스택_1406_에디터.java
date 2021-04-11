d121442e3bdf30bdd9051c3c85f6e2b8d6bf648d
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-11 13:46:24 +0900

    double stack

--- src/스택_1406_에디터.java
+++ src/스택_1406_에디터.java
@@ -1,46 +1,64 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class 스택_1406_에디터 {
+
+    static String line;
+    static int cursor;
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Character> s = new Stack<>();
-        Stack<Character> s2 = new Stack<>();
+        line = br.readLine();
+        cursor = line.length() - 2;
+        int n = Integer.parseInt(br.readLine());
 
-        char[] str = br.readLine().toCharArray();
-        int num = Integer.parseInt(br.readLine());
-        int StrLength = str.length;
-
-        for (int i = 0; i < StrLength; i++) {
-            s.push(str[i]);
-        }
-
-        for (int i = 0; i < num; i++) {
-
+        for (int i = 0; i < n; i++) {
             String[] cmd = br.readLine().split(" ");
 
-            if (cmd[0].equals("L") && !s.isEmpty()) {
-                s2.push(s.pop());
+            switch (cmd[0]) {
+                case "L":
+                    if (cursor > 0) cursor--;
+                    break;
+                case "D":
+                    if (cursor < line.length() - 2) cursor++;
+                    break;
+                case "B":
+                    B();
+                    break;
+                case "P":
+                    P(cmd[1]);
+                    break;
             }
-            if (cmd[0].equals("D") && !s2.isEmpty()) {
-                s.push(s2.pop());
+            /*
+            if (cmd[0] == "L") {
+                if (cursor > 0) cursor--;
             }
-            if (cmd[0].equals("B") && !s.isEmpty()) {
-                s.pop();
+            if (cmd[0] == "D") {
+                if (cursor < line.length() - 2) cursor++;
             }
-            if (cmd[0].equals("P")) {
-                s.push(cmd[1].charAt(0));
+            if (cmd[0] == "B") {
+                B();
             }
+            if (cmd[0] == "P") {
+                P(cmd[1]);
+            }*/
         }
 
-        while (!s2.isEmpty()) {
-            s.push(s2.pop());
-        }
+        System.out.println(line);
+    }
 
-        for (int i = 0; i < s.size(); i++) {
-            System.out.print(s.elementAt(i));
+    static void B() {
+        if (cursor > 2) {
+            line = line.substring(0, cursor - 2) + line.substring(cursor, line.length() - 1);
+        } else {
+            line = line.substring(0, 1) + line.substring(cursor, line.length() - 1);
+
         }
     }
+
+    static void P(String s) {
+        line = line.substring(0, cursor - 1).concat(s) + line.substring(cursor, line.length() - 1);
+    }
+
 }
