974c8df5a7a49452ca35c60521361ac7762c46a0
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-16 11:53:36 +0900

    reverse order

--- src/정렬_1427_소트인사이드.java
+++ src/정렬_1427_소트인사이드.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class 정렬_1427_소트인사이드 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        line.chars().map(e -> e - 48).sorted().forEach(System.out::print);
+        Arrays.stream(br.readLine().split("")).sorted(Comparator.comparing(String::valueOf).reversed()).forEach(System.out::print);
     }
 }
