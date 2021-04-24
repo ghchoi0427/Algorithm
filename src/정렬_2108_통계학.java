1a96120361e706f7ad17f32b81ffc6a84fca7c73
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-24 15:13:50 +0900

    feat: 최빈값, 범위 구현

--- src/정렬_2108_통계학.java
+++ src/정렬_2108_통계학.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class 정렬_2108_통계학 {
     public static void main(String[] args) throws Exception {
@@ -11,7 +13,13 @@ public class 정렬_2108_통계학 {
         for (int i = 0; i < num; i++) {
             list.add(Integer.parseInt(br.readLine()));
         }
-        System.out.println(list.stream().mapToInt(e -> e).average().orElseThrow(Exception::new));
-        list.stream().sorted().skip(num/2).limit(1).forEach(System.out::println);
+        int[] arr = new int[4000];
+        System.out.println((int) list.stream().mapToInt(e -> e).average().orElseThrow(Exception::new));
+        list.stream().sorted().skip(num / 2).limit(1).forEach(System.out::println);
+        list.stream().filter(e -> e > 0).forEach(e -> arr[e]++);
+        System.out.println(Arrays.stream(arr).max().orElseThrow(NoSuchElementException::new));
+        System.out.println(list.stream().max().orElseThrow(NoSuchElementException::new)
+                - list.stream().min().orElseThrow(NoSuchElementException::new));
+
     }
 }
