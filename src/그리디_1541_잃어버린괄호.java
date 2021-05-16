28ddea404c93aa8fae3ebd5e59a8ba2f1be0fac2
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-16 23:05:54 +0900

    feat: 함수 구현 '-'로 split한 첫번째 그룹에서 나머지 그룹을 모두 뺀다.

--- src/그리디_1541_잃어버린괄호.java
+++ src/그리디_1541_잃어버린괄호.java
@@ -5,17 +5,18 @@ import java.util.Arrays;
 public class 그리디_1541_잃어버린괄호 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        String[] subtract = input.split("-");
+        String[] subtract = br.readLine().split("-");
         boolean isFirstGroup = true;
+        int result = 0;
 
-
-        int firstNumber = Integer.parseInt(subtract[0]);
-        subtract[0] = "0";
         for (String s : subtract) {
-            String[] temp = s.split("\\+");
-
+            if (isFirstGroup) {
+                result += Arrays.stream(s.split("\\+")).mapToInt(Integer::parseInt).sum();
+                isFirstGroup = false;
+            } else {
+                result -= Arrays.stream(s.split("\\+")).mapToInt(Integer::parseInt).sum();
+            }
         }
-        System.out.println(firstNumber);
+        System.out.println(result);
     }
 }
