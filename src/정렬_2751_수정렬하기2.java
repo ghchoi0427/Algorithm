dcdeeb2d03e47eab8ad4d05801cddffe93fab316
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-17 23:56:00 +0900

    initial

---
+++ src/정렬_2751_수정렬하기2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class 정렬_2751_수정렬하기2 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i] + "\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
