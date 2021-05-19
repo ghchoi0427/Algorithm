b496264f3d6e5f82d1c9bdd827dfc7689dba2790
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-19 23:45:47 +0900

    feat: getter 추가

--- src/백트래킹_15649_N과M_1.java
+++ src/백트래킹_15649_N과M_1.java
@@ -8,30 +8,44 @@ public class 백트래킹_15649_N과M_1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+        // int m = Integer.parseInt(input[1]);
         List<Node> nodes = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
             nodes.add(new Node(i));
         }
+
+        for (Node node : nodes) {
+            node.addNode(nodes);
+        }
     }
 }
 
 class Node {
-    List<Node> connected;
+    private List<Node> connected;
     private int number;
-
-    public Node() {
-    }
 
     public Node(int number) {
         this.number = number;
     }
 
+    public int getNumber() {
+        return number;
+    }
+
     public void addNode(Node node) {
-        if (!node.equals(this)) {
-            connected.add(node);
+        connected.add(node);
+    }
+
+    public void addNode(List<Node> nodeList) {
+        for (Node n : nodeList) {
+            if (n.getNumber() != this.number) {
+                connected.add(n);
+            }
         }
     }
-}
 
+    public List<Node> getConnected() {
+        return connected;
+    }
+}
