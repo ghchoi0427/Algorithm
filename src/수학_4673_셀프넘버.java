29f5f03599c5d31088fb21ff6c2511e767a31f7d
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-24 23:13:29 +0900

    refactor: d 메서드 stream으로 변경

--- src/수학_4673_셀프넘버.java
+++ src/수학_4673_셀프넘버.java
@@ -1,22 +1,18 @@
+import java.util.stream.IntStream;
+
 public class 수학_4673_셀프넘버 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         for (int i = 1; i <= 10000; i++) {
-            if (isH(i) == false) {
+            if (!isH(i)) {
                 System.out.println(i);
             }
         }
     }
 
     public static int d(int n) {
-        int result = n;
-        while (n > 0) {
-            result += n % 10;
-            n /= 10;
-        }
-        return result;
+        return Integer.toString(n).chars().mapToDouble(e->e-48).mapToInt(e->(int)e).sum()+n;
     }
-
 
     static boolean isH(int n) {
         for (int j = 1; j < n; j++) {
