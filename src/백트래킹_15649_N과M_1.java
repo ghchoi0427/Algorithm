8edbfabf85afa37e14ae4a8da67e106fc3f1c0cc
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-27 17:27:33 +0900

    fix: 필요없는 메서드 제거

--- src/백트래킹_15649_N과M_1.java
+++ src/백트래킹_15649_N과M_1.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class 백트래킹_15649_N과M_1 {
     static List<Node> nodes = new ArrayList<>();
@@ -10,22 +12,20 @@ public class 백트래킹_15649_N과M_1 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+
         int n = Integer.parseInt(input[0]);
-        // int m = Integer.parseInt(input[1]);
+        int m = Integer.parseInt(input[1]);
 
         for (int i = 1; i <= n; i++) {
             nodes.add(new Node(i));
         }
 
-    }
-
-    static Node findNodeById(int id) {
-        for (Node n : nodes) {
-            if (n.getNumber() == id) {
-                return n;
-            }
+        for (Node node : nodes) {
+            node.setConnected(nodes.stream().filter(e -> !e.equals(node)).collect(Collectors.toList()));
         }
-        return new Node(id);
+
+        nodes.stream().forEach(System.out::println);
+
     }
 }
 
