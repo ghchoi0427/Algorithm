47c8df8027fd5fecc1790454fce3944ff6d215f2
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-28 11:47:33 +0900

    refactor: stream 으로 변경

--- src/백트래킹_15649_N과M_1.java
+++ src/백트래킹_15649_N과M_1.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class 백트래킹_15649_N과M_1 {
     static List<Node> nodes = new ArrayList<>();
@@ -16,15 +16,12 @@ public class 백트래킹_15649_N과M_1 {
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
 
-        for (int i = 1; i <= n; i++) {
-            nodes.add(new Node(i));
-        }
+        IntStream.rangeClosed(1, n).mapToObj(e -> nodes.add(new Node(e)));
 
         for (Node node : nodes) {
             node.setConnected(nodes.stream().filter(e -> !e.equals(node)).collect(Collectors.toList()));
         }
 
-        nodes.stream().forEach(System.out::println);
 
     }
 }
