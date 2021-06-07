39b014e138927d5e309d04cbe8e47c704f23df5b
ghchoi0427 <ghchoi0427@gmail.com>
2021-06-07 19:48:43 +0900

    initial commit

---
+++ src/브루트포스_2798_블랙잭.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class 브루트포스_2798_블랙잭 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        int max = 0;
+        String[] cardsStr = sc.nextLine().split(" ");
+        int[] cards = new int[cardsStr.length];
+        for (int i = 0; i < cardsStr.length; i++) {
+            cards[i] = Integer.parseInt(cardsStr[i]);
+        }
+        for (int i = 0; i < cards.length - 2; i++) {
+            for (int j = i + 1; j < cards.length - 1; j++) {
+                for (int k = j + 1; k < cards.length; k++) {
+                    if (cards[i] + cards[j] + cards[k] <= M) {
+                        if (max < cards[i] + cards[j] + cards[k]) {
+                            max = cards[i] + cards[j] + cards[k];
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
