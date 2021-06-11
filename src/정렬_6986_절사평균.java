39a36732e2a033343122775aa858bc64de7f98db
ghchoi0427 <ghchoi0427@gmail.com>
2021-06-11 15:01:10 +0900

    feat: 보정평균

--- src/정렬_6986_절사평균.java
+++ src/정렬_6986_절사평균.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class 정렬_6986_절사평균 {
     public static void main(String[] args) throws Exception {
@@ -10,11 +8,27 @@ public class 정렬_6986_절사평균 {
         int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int n = line[0];
         int k = line[1];
+
         List<Float> list = new ArrayList<>();
+        List<Float> newList = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             list.add(Float.parseFloat(br.readLine()));
         }
-        System.out.println(String.format("%.2f",list.stream().sorted().skip(k).limit(n - 2 * k).mapToDouble(Float::doubleValue).average().orElseThrow(Exception::new)));
+
+        Collections.sort(list);
+        System.out.println(String.format("%.2f", list.stream().skip(k).limit(n - 2 * k).mapToDouble(Float::doubleValue).average().orElseThrow(Exception::new)));
+
+        for (int i = 0; i < n; i++) {
+            if (i < k) {
+                newList.add(list.get(k));
+            } else if (k <= i && i < n - k) {
+                newList.add(list.get(i));
+            } else if (n - k <= i) {
+                newList.add(list.get(n - k - 1));
+            }
+        }
+        System.out.println(String.format("%.2f", newList.stream().mapToDouble(Float::doubleValue).average().orElseThrow(Exception::new)));
+
     }
 }
