08a8b6d69f6b00df682cec7b8b32975994874b3f
ghchoi0427 <ghchoi0427@gmail.com>
2021-06-11 14:33:24 +0900

    initial commit

---
+++ src/정렬_6986_절사평균.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 정렬_6986_절사평균 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = line[0];
+        int k = line[1];
+        List<Float> list = new ArrayList<>();
+
+    }
+}
