1eaa78b0bd526c65fabc6ab852870e6fc5175be4
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-07 21:15:09 +0900

    [ADD] 숫자게임

---
+++ python/programmers/level3/숫자게임.py
@@ -0,0 +1,16 @@
+def solution(A,B):
+  answer = 0
+
+  A.sort(reverse = True)
+  B.sort(reverse = True)
+  
+  ac = bc = 0
+
+  while(bc < len(B) and ac<len(A) and bc < len(B)):
+    if(A[ac] < B[bc]):
+      answer += 1
+      bc += 1
+      ac += 1
+    else:
+      ac += 1
+  return answer
