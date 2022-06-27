b83062f9ae5099c5af280562d9462af2e43ffd53
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-27 13:12:35 +0900

    [MOD] 가장 큰 수, 두번째 큰 수 구하는 방법 간략화

--- python/dongbinbook/greedy/큰수의법칙.py
+++ python/dongbinbook/greedy/큰수의법칙.py
@@ -3,32 +3,21 @@ d = [0 * i for i in range(n)]
 li = list(map(int, input().split()))
 for i in range(len(li)):
   d[i]=li[i]
-secondindex = 0
-maxindex = 0
 result = 0
-for i in range(len(d)):
-  if(d[i] == max(d)):
-    maxindex = i
-    break
 
-secondmax = -1
-for i in range(len(d)):
-  if(i != maxindex):
-    if(secondmax < d[i]):
-      secondmax = d[i]
-      secondindex = i
-
-count = k
+d.sort()
+maxnum = d[-1]
+secondmax = d[-2]
 
 while(True):
   for i in range(k):
-    result += d[maxindex]
+    result += maxnum
     m -= 1
     if(m<=0):
       break
   if(m<=0):
     break
-  result += d[secondindex]
+  result += secondmax
   m -= 1
   if(m<=0):
     break
